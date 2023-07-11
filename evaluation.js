2e4f03f76851284de2800e33c812125f48b81ffe
Bruce Webster <brucewebster222@gmail.com>
2023-07-11 12:14:32 -0400

    14th commit

--- evaluation.js
+++ evaluation.js
@@ -1,19 +1,23 @@
-const form = document.getElementById('form')
-const correct1 = document.getElementById("correct1")
-const correct2 = document.getElementById("correct2")
-const correct3 = document.getElementById("correct3")
+const form = document.getElementById("form");
+const correct1 = document.getElementById("correct-1");
+const correct2 = document.getElementById("correct-2");
+const correct3 = document.getElementById("correct-3");
+const score = document.getElementById("score");
+const button = document.getElementById("button");
+const array = [correct1, correct2, correct3];
 
-const array = [ correct1, correct2 , correct3]
-let count = 0
+let count = 0;
 
 function handleSubmit(e) {
-  e.preventDefault()
+  e.preventDefault();
   for (let i = 0; i < array.length; i++) {
     if (array[i].checked) {
       count++;
     }
   }
-  console.log(count)
+
+  score.innerHTML = count;
+  button.style.display = "none";
 }
 
-form.addEventListener('submit', handleSubmit)
+form.addEventListener("submit", handleSubmit);
