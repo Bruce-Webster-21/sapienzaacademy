613499d773c9739d2e8b439b87d40d60f71794de
Bruce Webster <brucewebster222@gmail.com>
2023-07-12 15:26:03 -0400

    15th commit

--- evaluation.js
+++ evaluation.js
@@ -1,20 +1,19 @@
 const form = document.getElementById("form");
 const correct1 = document.getElementById("correct-1");
-const correct2 = document.getElementById("correct-2");
-const correct3 = document.getElementById("correct-3");
 const score = document.getElementById("score");
 const button = document.getElementById("button");
-const array = [correct1, correct2, correct3];
+const array = [correct1];
 
 let count = 0;
 
 function handleSubmit(e) {
   e.preventDefault();
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].checked) {
+
+  array.forEach((answer) => {
+    if (answer.checked) {
       count++;
     }
-  }
+  });
 
   score.innerHTML = count;
   button.style.display = "none";
